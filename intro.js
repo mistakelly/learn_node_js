6759be3a8e6ede03825f8b1211ea30b2590c83d5
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-08-01 05:34:00 +0100

    introduction to node.js

---
+++ intro.js
@@ -0,0 +1,19 @@
+// console.log('JESUS IS THE GOD')
+const fs= require('fs')
+
+
+
+
+// const data = fs.readFile('./example.txt',  (err, data) => {
+//     if (err){
+//         console.log('error occure', err)
+//     }else{
+//         console.log('my data dee body', data)
+//     }
+// })
+
+// // console.log(data)
+
+// console.log('JESUS IS THE LORD')
+
+
