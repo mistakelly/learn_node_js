685ec9b10f6cbd23c9b643c08f3b63d0ed69ecb9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-08-13 12:30:00 +0100

    Discovered fs.writeFile() to asynchronously and synchronous write data to a file, allowing smooth, non-blocking  and blocking operations

--- scripts/fs.js
+++ scripts/fs.js
@@ -6,3 +6,18 @@ fs.readFile('file.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log('File data (async):', data);
 });
+
+
+// 2. fs.readFileSync() - Synchronously reads the contents of a file
+try {
+    const data = fs.readFileSync('file.txt', 'utf8');
+    console.log('File data (sync):', data);
+} catch (err) {
+    console.error(err);
+}
+
+// 3. fs.writeFile() - Asynchronously writes data to a file
+fs.writeFile('output.txt', 'Hello, world!', 'utf8', (err) => {
+    if (err) throw err;
+    console.log('File has been saved!');
+});
