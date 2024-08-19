859ff5b84253164d5a43df1326620eed754e79dd
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-08-19 04:30:00 +0100

    Set up HTTP server to serve file content after reading it asynchronously using streams.
    - Created a read stream for index.html to read file content in chunks.
    - Accumulated the data and created an HTTP server that listens for GET requests.
    - Served the file content as the response to incoming requests.
    
    This approach ensures the server only starts after the file is fully read.

--- scripts/http.js
+++ scripts/http.js
@@ -1,20 +1,48 @@
 const http = require('http')
 
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
 
-    if (res.statusCode == 200){
-        console.log('valid response')
-    }else{
-        console.log('error')
-    }
+//     if (res.statusCode == 200){
+//         console.log('valid response')
+//     }else{
+//         console.log('error')
+//     }
 
-    res.setHeader('content-Type', 'Applicatieewfwaon/json')
-    res.end('hello world from http module')
-})
+//     res.setHeader('content-Type', 'Applicatieewfwaon/json')
+//     res.end('hello world from http module')
+// })
 
 
 
-server.listen(3000, ()=> {
-    console.log('listening on port 3000')
+// server.listen(3000, ()=> {
+//     console.log('listening on port 3000')
+// })
+
+
+
+
+const fs = require('fs')
+
+const readStream = fs.createReadStream('index.html', 'utf-8')
+
+let data = ''
+
+readStream.on('data', chunk => {
+    data += chunk
+}).on('end', () => {
+    const server = http.createServer((req, res) => {
+        console.log(`${req.method} request received at ${req.url}`);
+
+        if (req.method == 'GET') {
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/plain')
+
+            res.end(data)
+        }
+    })
+
+    server.listen(3000, () => {
+        process.stdout.write('listening on port 3000> ')
+    })
 })
